c9253bc5f40f39e970b67e0783e2cf0395f94c1f
ginodev <ginodev24@gmail.com>
2024-05-02 15:48:42 -0700

    Mejora de importaciones, index.js, archivos de barril

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+//El index.js sirve para agrupar todos los elementos que se quieren exportar
+//para que no haya necesidad de hacerlo en todos los archivos explícitamente
+
+export * from "./AddCategory";
+export * from "./GifGrid";
+export * from "./GifItem";
